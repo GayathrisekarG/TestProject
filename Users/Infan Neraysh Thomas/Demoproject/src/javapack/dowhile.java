d2489b72b6287f0d57de5f63fb847775b2155591
Gayathrisekar13 <gayathrineraysh@gmail.com>
2022-12-08 18:06:36 +0530

    first commit

---
+++ Users/Infan Neraysh Thomas/Demoproject/src/javapack/dowhile.java
@@ -0,0 +1,13 @@
+package javapack;
+
+public class dowhile {
+	
+	public static void main(String[] args) {
+		int i = 1;  //initialization
+		do {
+			System.out.println(i);
+			i++; //increment i++ = i+1
+		} while (i <= 10);//test condition
+	}
+
+}
